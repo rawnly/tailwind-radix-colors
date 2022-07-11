4b76075c41e6d3356aa7db5fcaf00d96e9ecb473
Federico Vitale <fedevitale99@gmail.com>
2022-07-11 15:36:54 +0200

    feat: custom colors support

--- apps/tailwind-radix-colors/src/radix-tailwind.ts
+++ apps/tailwind-radix-colors/src/radix-tailwind.ts
@@ -4,19 +4,27 @@ import * as radixColors from '@radix-ui/colors'
 
 export const colors = toTailwindColors( radixColors )
 
+type ColorType = {
+	[color: string]: Record<string, string>
+} & {
+	[color: `${string}Dark`]: Record<string, string>
+}
+
 type Options = {
 	prefix?: string
 	properties?: string[]
+	colors: ColorType;
 }
 
 export const plugin = twPlugin.withOptions<Options>( ( options = {} ) =>
 	( { addUtilities, e, theme } ) => {
 		const {
 			prefix = 'rx',
-			properties = ['bg', 'shadow', 'text', 'border', 'ring']
+			properties = ['bg', 'shadow', 'text', 'border', 'ring'],
+			colors = radixColors
 		} = options
 
-		const utilities = Object.keys( radixColors )
+		const utilities = Object.keys( colors )
 			.filter( color => !( /Dark|A|black|white$/.test( color ) ) )
 			.reduce( ( acc, colorName ) => {
 				const colorStyles = Array.from( new Array( 12 ) ).reduce( ( acc, _, idx ) => {
